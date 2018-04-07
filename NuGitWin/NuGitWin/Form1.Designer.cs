0e8eccc1e4aca2c1f497cc35beba3b7e7484d7be
Ryan Herman <ryan.herman@1iota.com>
2018-04-06 19:51:18 -0700

    Latest

---
+++ NuGitWin/NuGitWin/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace NuGitWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.GitSource = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.StoreSource = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SolutionDir = new System.Windows.Forms.TextBox();
+            this.SourceDir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(156, 288);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(276, 64);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GitSource
+            // 
+            this.GitSource.Location = new System.Drawing.Point(56, 111);
+            this.GitSource.Name = "GitSource";
+            this.GitSource.Size = new System.Drawing.Size(496, 26);
+            this.GitSource.TabIndex = 1;
+            this.GitSource.Text = "https://github.com/jamesmontemagno/MediaPlugin.git";
+            // 
+            // StoreSource
+            // 
+            this.StoreSource.Location = new System.Drawing.Point(56, 66);
+            this.StoreSource.Name = "StoreSource";
+            this.StoreSource.Size = new System.Drawing.Size(496, 26);
+            this.StoreSource.TabIndex = 2;
+            this.StoreSource.Text = "C:\\temp\\collectioncrash\\NuGitSource\\Segme\\nuget";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(156, 374);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(276, 66);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Explore";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(637, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1498, 236);
+            this.listBox1.TabIndex = 4;
+            // 
+            // SolutionDir
+            // 
+            this.SolutionDir.Location = new System.Drawing.Point(56, 156);
+            this.SolutionDir.Name = "SolutionDir";
+            this.SolutionDir.Size = new System.Drawing.Size(496, 26);
+            this.SolutionDir.TabIndex = 5;
+            this.SolutionDir.Text = "C:\\temp\\collectioncrash\\collectioncrash.sln";
+            // 
+            // SourceDir
+            // 
+            this.SourceDir.Location = new System.Drawing.Point(56, 209);
+            this.SourceDir.Name = "SourceDir";
+            this.SourceDir.Size = new System.Drawing.Size(496, 26);
+            this.SourceDir.TabIndex = 6;
+            this.SourceDir.Text = "C:\\temp\\collectioncrash\\NuGitSource\\Segme\\SegmentedControl";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2187, 523);
+            this.Controls.Add(this.SourceDir);
+            this.Controls.Add(this.SolutionDir);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.StoreSource);
+            this.Controls.Add(this.GitSource);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox GitSource;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox StoreSource;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox SolutionDir;
+        private System.Windows.Forms.TextBox SourceDir;
+    }
+}
+
---
+++ NuGitWin/NuGitWin/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+using NuGet;
+
+namespace NuGitWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+          
+            if (string.IsNullOrEmpty(StoreSource.Text))
+            {
+                folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StoreSource.Text = folderBrowserDialog1.SelectedPath;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            int startPos = GitSource.Text.LastIndexOf('/');
+            int endPos = GitSource.Text.LastIndexOf(".git");
+            var FolderName = GitSource.Text.Substring(startPos + 1, GitSource.Text.Length - endPos + 1);
+
+            var co = new CloneOptions();
+            //co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "Username", Password = "Password" };
+            Repository.Clone(GitSource.Text, StoreSource.Text + "\\NuGitSource\\" + FolderName, co);
+            
+
+
+        }
+
+        private async void AddButton_Click(object sender, EventArgs e)
+        {
+            PackageBuilder builder;
+            string AbstractionDIR = string.Empty;
+            string DroidDIR = string.Empty;
+            string iOS = string.Empty;
+
+
+            //  File.WriteAllText(SolutionDir.Text, text);
+            listBox1.Items.Clear();
+
+            if (!string.IsNullOrEmpty(StoreSource.Text))
+                openFileDialog1.InitialDirectory = StoreSource.Text;
+
+
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                var packagePath = openFileDialog1.FileName;
+
+                using (var str = ManifestUtility.ReadManifest(packagePath))
+                {
+                    builder = new PackageBuilder(str, Path.GetDirectoryName(packagePath));
+
+                    int counter = 0;
+                    foreach (var eachFile in builder.Files)
+                    {
+                        Debug.WriteLine(eachFile.Path);
+                        var filePath = eachFile.EffectivePath.ToLower();
+
+                        var framework = eachFile.Path.Split('/');
+                        var projectName = framework[2].Replace(".dll", "");
+
+
+                        if (filePath.Contains("netstandard"))
+                        {
+                            listBox1.Items.Add(filePath + "|" + "NETStandard|" + framework[1] + "|" + projectName);
+
+                        }
+
+                        if (filePath.Contains("portable"))
+                        {
+                            //  listBox1.Items.Add("  PCL - " + framework[1]);
+                        }
+
+                        if (eachFile.Path.Contains("MonoAndroid") && !eachFile.Path.Contains("+"))
+                        {
+                            listBox1.Items.Add(filePath + "|" + "Android|" + framework[1] + "|" + projectName);
+
+                        }
+
+                        if (eachFile.Path.Contains("Xamarin.iOS") && !eachFile.Path.Contains("+"))
+                        {
+                            listBox1.Items.Add(filePath + "|" + "iOS|" + framework[1] + "|" + projectName);
+                        }
+
+                        counter++;
+                        if ((counter % 2) == 0)
+                        {
+
+                            // continue;
+                        }
+
+                    }
+                }
+
+                Debug.WriteLine("Hi: " + builder);
+            }
+
+            string text = File.ReadAllText(SolutionDir.Text);
+            string insertPoint = "EndProject";
+            int findPoint = text.LastIndexOf(insertPoint);
+            int index = findPoint + insertPoint.Length;
+            var insertText = string.Empty;
+            Guid g;
+            Guid g2;
+
+            foreach (var eachInsert in listBox1.Items)
+            {
+                g = Guid.NewGuid();
+                g2 = Guid.NewGuid();
+                var splitIt = eachInsert.ToString().Split('|');
+
+                insertText = insertText + $"\nProject(\"{{" + g +
+                             "}}\") = \"" + splitIt[3] + "\", \"" + SourceDir.Text + "\\" + splitIt[3] + "\\" +
+                             splitIt[3] + ".csproj\", \"{{" +
+                             g2 + "}}\"\nEndProject\n";
+            }
+
+            var finalText = text.Insert(index, insertText);
+
+            File.WriteAllText(SolutionDir.Text, finalText);
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                var droidPath = openFileDialog1.FileName;
+                string insertDroid = string.Empty;
+
+                string text2 = File.ReadAllText(droidPath);
+                string insertPoint2 = "ItemGroup";
+                int findPoint2 = text2.LastIndexOf(insertPoint2);
+                int index2 = findPoint2 + insertPoint2.Length + 1;
+                Guid rando;
+                foreach (var eachInsert2 in listBox1.Items)
+                {
+                    if (eachInsert2.ToString().Contains("MonoAndroid"))
+                    {
+                        rando = Guid.NewGuid();
+                        var splitIt = eachInsert2.ToString().Split('|');
+                        var middleDroid = SourceDir.Text + "\\" + splitIt[3] + "\\" + splitIt[3] + ".csproj";
+                        var fullInput =
+                            $"     <ProjectReference Include=\"" + middleDroid + "\">\r\n      <Project>{" + rando + "}</Project>\r\n      <Name>" + splitIt[3] + "</Name>\n</ProjectReference>\n";
+                        insertDroid = insertDroid + "<ItemGroup>\n" + fullInput + "</ItemGroup>\n";
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(insertDroid))
+                {
+                    var finalText2 = text2.Insert(index2, "\n" + insertDroid);
+
+                    File.WriteAllText(droidPath, finalText2);
+                }
+            }
+        }
+    }
+}
+
